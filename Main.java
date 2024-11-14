f69cf635cdf046b454b3fd846cfe93e748423749
S3V3N7HP47H <24013757@student.hhs.nl>
2024-11-14 13:24:20 +0100

    0.0.0.0.1

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main ( String [] args) {
         System.out.println("hello world");
         System.out.println("Lucas Baas");
-        System.out.println("VIncent is aan het werken!");
+        System.out.println("Vincent is aan het werken!");
+        Scanner scanner = new Scanner(System.in);
+        String hallo = scanner.nextLine();
+        System.out.print(hallo);
+
     }
 }
 //hier test commit vincent//
